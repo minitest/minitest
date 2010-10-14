ce4ed2672cec5e2ebc7f9894099d3912b5e385ff
Ryan Davis <ryand@zenspider.com>
2010-10-14 02:11:46 -0800

    refactored the tests. gorgeous. also, I love autotest [git-p4: depot-paths = "//src/minitest/dev/": change = 5898]

--- test/test_minitest_benchmark.rb
+++ test/test_minitest_benchmark.rb
@@ -7,35 +7,34 @@ require 'minitest/benchmark'
 
 class TestMiniTestBenchmark < MiniTest::Unit::TestCase
   def test_fit_exponential_clean
-    util_fit_exponential :fit_exponential
+    x = [1.0, 2.0, 3.0, 4.0, 5.0]
+    y = x.map { |n| 1.1 * Math.exp(2.1 * n) }
+
+    assert_fit :exponential, x, y, 1.0, 1.1, 2.1
   end
 
   def test_fit_exponential_noisy
-    t = [1.0, 1.9, 2.6, 3.4, 5.0]
-    p = [12, 10, 8.2, 6.9, 5.9]
+    x = [1.0, 1.9, 2.6, 3.4, 5.0]
+    y = [12, 10, 8.2, 6.9, 5.9]
 
-    a, b, rr = fit_exponential(t, p)
-
-    assert_operator rr, :>, 0.95
-    assert_in_delta 13.812, a
-    assert_in_delta(-0.182, b)
+    assert_fit :exponential, x, y, 0.95, 13.812, -0.182
   end
 
   def test_fit_exponential_weighted_clean
-    util_fit_exponential :fit_exponential_weighted
+    x = [1.0, 2.0, 3.0, 4.0, 5.0]
+    y = x.map { |n| 1.1 * Math.exp(2.1 * n) }
+
+    assert_fit :exponential_weighted, x, y, 1.0, 1.1, 2.1
   end
 
   def test_fit_exponential_weighted_noisy
     # from: http://reference.wolfram.com/mathematica/ref/FindFit.html
     # http://www.wolframalpha.com/input/?i=exponential+fit+%7B1.0%2C+12%7D%2C+%7B1.9%2C+10%7D%2C+%7B2.6%2C+8.2%7D%2C+%7B3.4%2C+6.9%7D%2C+%7B5.0%2C+5.9%7D
-    t = [1.0, 1.9, 2.6, 3.4, 5.0]
-    p = [12, 10, 8.2, 6.9, 5.9]
+    x = [1.0, 1.9, 2.6, 3.4, 5.0]
+    y = [12, 10, 8.2, 6.9, 5.9]
 
-    a, b, rr = fit_exponential_weighted(t, p)
-
-    assert_operator rr, :>, 0.95
-    assert_in_delta 14.3889, a, 0.4 # ugh. I dunno what's going on
-    assert_in_delta(-0.198208, b, 0.01)
+    assert_fit :exponential_weighted, x, y, 0.95, 14.099, -0.1889
+    # FIX: a, b = 14.3889, -0.198208 according to above url
   end
 
   def test_fit_linear_clean
@@ -43,19 +42,14 @@ class TestMiniTestBenchmark < MiniTest::Unit::TestCase
     x = (1..5).to_a
     y = x.map { |n| 2.2 * n + 3.1 }
 
-    m, b, rr = fit_linear(x, y)
-    assert_in_delta 2.2, m
-    assert_in_delta 3.1, b
-    assert_operator rr, :>, 0.9999999
+    assert_fit :linear, x, y, 1.0, 2.2, 3.1
   end
 
   def test_fit_linear_noisy
     x = [ 60,  61,  62,  63,  65]
     y = [3.1, 3.6, 3.8, 4.0, 4.1]
-    m, b, rr = fit_linear(x, y)
-    assert_in_delta  0.188, m
-    assert_in_delta(-7.964, b)
-    assert_operator rr, :>, 0.8
+
+    assert_fit :linear, x, y, 0.83, 0.188, -7.964
   end
 
   def test_fit_power_clean
@@ -65,11 +59,7 @@ class TestMiniTestBenchmark < MiniTest::Unit::TestCase
     x = [1.0, 2.0, 3.0, 4.0, 5.0]
     y = [1.0, 4.0, 9.0, 16.0, 25.0]
 
-    a, b, rr = fit_power(x, y)
-
-    assert_in_delta 1.0, a
-    assert_in_delta 2.0, b
-    assert_in_delta 1.0, rr
+    assert_fit :power, x, y, 1.0, 1.0, 2.0
   end
 
   def test_fit_power_noisy
@@ -77,25 +67,16 @@ class TestMiniTestBenchmark < MiniTest::Unit::TestCase
     x = [10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 35]
     y = [95, 105, 125, 141, 173, 200, 253, 298, 385, 459, 602]
 
-    a, b, rr = fit_power(x, y)
-
-    assert_in_delta 2.621, a
-    assert_in_delta 1.456, b
-    # assert_in_delta 0.93553, rr
-    # FIX: this error alg is wrong according to url above
-    assert_operator rr, :>, 0.9
+    assert_fit :power, x, y, 0.9, 2.621, 1.456
+    # FIX: fit should be 0.93553, url above says my alg is wrong
   end
 
-  def util_fit_exponential msg
-    # y = a * e^(b*x), where a = 1.1, b = 2.1
-    x = [1.0, 2.0, 3.0, 4.0, 5.0]
-    y = x.map { |n| 1.1 * Math.exp(2.1 * n) }
+  def assert_fit msg, x, y, fit, exp_a, exp_b
+    a, b, rr = send "fit_#{msg}", x, y
 
-    a, b, rr = send(msg, x, y)
-
-    assert_in_delta 1.1, a
-    assert_in_delta 2.1, b
-    assert_operator rr, :>, 0.9999999
+    assert_operator rr, :>=, fit
+    assert_in_delta exp_a, a
+    assert_in_delta exp_b, b
   end
 end
 
