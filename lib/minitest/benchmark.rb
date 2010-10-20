ef744a1eff4be606a666ecd58bf9c2834a98e15f
Ryan Davis <ryand@zenspider.com>
2010-10-20 14:57:28 -0800

    Removed weighted exponential fit since I couldn't verify it anywhere, including wolfram alpha. Verified all test numbers and commented on verification sources. [git-p4: depot-paths = "//src/minitest/dev/": change = 5903]

--- lib/minitest/benchmark.rb
+++ lib/minitest/benchmark.rb
@@ -12,6 +12,7 @@ class MiniTest::Unit
     def self.bench_exp min, max, base = 10
       min = (Math.log10(min) / Math.log10(base)).to_i
       max = (Math.log10(max) / Math.log10(base)).to_i
+
       (min..max).map { |m| base ** m }.to_a
     end
 
@@ -175,14 +176,14 @@ class MiniTest::Unit
 
     def fit_error xys
       y_bar  = sigma(xys) { |x, y| y } / xys.size.to_f
-      ss_tot = sigma(xys) { |x, y| (y - y_bar) ** 2 }
-      ss_err = sigma(xys) { |x, y| (yield(x) - y)**2 }
+      ss_tot = sigma(xys) { |x, y| (y    - y_bar) ** 2 }
+      ss_err = sigma(xys) { |x, y| (yield(x) - y) ** 2 }
 
       1 - (ss_err / ss_tot)
     end
 
     ##
-    # To fit a functional form: y = ae^(bx), without weighting.
+    # To fit a functional form: y = ae^(bx).
     #
     # Takes x and y values and returns [a, b, r^2].
     #
@@ -195,40 +196,16 @@ class MiniTest::Unit
       slny  = sigma(xys) { |x,y| Math.log(y)     }
       sx2   = sigma(xys) { |x,y| x * x           }
       sx    = sigma xs
-      sy    = sigma ys
 
-      d = n * sx2 - sx ** 2
-      a = (slny * sx2 - sx * sxlny) / d
-      b = (n * sxlny - sx * slny)   / d
-
-      return Math.exp(a), b, fit_error(xys) { |x| Math.exp(a + b * x) }
-    end
-
-    ##
-    # To fit a functional form: y = ae^(bx), with weighting.
-    #
-    # Takes x and y values and returns [a, b, r^2].
-    #
-    # See: http://mathworld.wolfram.com/LeastSquaresFittingExponential.html
-
-    def fit_exponential_weighted xs, ys
-      n      = xs.size
-      sy     = sigma ys
-      xys    = xs.zip(ys)
-      sx2y   = sigma(xys) { |x,y| x * x * y           }
-      sxy    = sigma(xys) { |x,y| x * y               }
-      sxylny = sigma(xys) { |x,y| x * y * Math.log(y) }
-      sylny  = sigma(ys)  { |y| y * Math.log(y)       }
-
-      # A = Exp(a), B = b
-      d = sy * sx2y - sxy**2
-      a, b = (sx2y * sylny - sxy * sxylny) / d, (sy * sxylny - sxy * sylny) / d
+      c = n * sx2 - sx ** 2
+      a = (slny * sx2 - sx * sxlny) / c
+      b = ( n * sxlny - sx * slny ) / c
 
       return Math.exp(a), b, fit_error(xys) { |x| Math.exp(a + b * x) }
     end
 
     ##
-    # Fits the functional form: ax + b.
+    # Fits the functional form: a + bx.
     #
     # Takes x and y values and returns [a, b, r^2].
     #
@@ -237,18 +214,16 @@ class MiniTest::Unit
     def fit_linear xs, ys
       n   = xs.size
       xys = xs.zip(ys)
-      xy  = xys.map { |x,y| x * y  }
-      xx  = xs.map  { |x|   x ** 2 }
       sx  = sigma xs
       sy  = sigma ys
-      sxy = sigma xy
-      sxx = sigma xx
+      sx2 = sigma(xs)  { |x|   x ** 2 }
+      sxy = sigma(xys) { |x,y| x * y  }
 
-      # calculate slope and intercept
-      m = ((n * sxy) - (sx * sy)) / ((n * sxx) - (sx ** 2))
-      b = (sy - (m * sx)) / n
+      c = n * sx2 - sx**2
+      a = (sy * sx2 - sx * sxy) / c
+      b = ( n * sxy - sx * sy ) / c
 
-      return m, b, fit_error(xys) { |x| m * x + b }
+      return a, b, fit_error(xys) { |x| a + b * x }
     end
 
     ##
@@ -259,15 +234,15 @@ class MiniTest::Unit
     # See: http://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html
 
     def fit_power xs, ys
-      n = xs.size
-      xys = xs.zip(ys)
+      n       = xs.size
+      xys     = xs.zip(ys)
       slnxlny = sigma(xys) { |x, y| Math.log(x) * Math.log(y) }
-      slnx = sigma(xs) { |x| Math.log(x) }
-      slny = sigma(ys) { |y| Math.log(y) }
-      slnx2 = sigma(xs) { |x| Math.log(x) ** 2 }
+      slnx    = sigma(xs)  { |x   | Math.log(x)               }
+      slny    = sigma(ys)  { |   y| Math.log(y)               }
+      slnx2   = sigma(xs)  { |x   | Math.log(x) ** 2          }
+
       b = (n * slnxlny - slnx * slny) / (n * slnx2 - slnx ** 2);
       a = (slny - b * slnx) / n
-      sy    = sigma ys
 
       return Math.exp(a), b, fit_error(xys) { |x| (Math.exp(a) * (x ** b)) }
     end
@@ -323,8 +298,6 @@ end
 class MiniTest::Spec
   def self.bench name, &block
     define_method "bench_#{name.gsub(/\W+/, '_')}", &block
-    # define_method "bench_#{name.gsub(/\W+/, '_')}" do block.call end
-    # define_method("bench_#{name.gsub(/\W+/, '_')}") { self.instance_eval(&block) }
   end
 
   def self.bench_range &block
