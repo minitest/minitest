a19105c01779da5d77120c8ac26f054a4d8205dc
Ryan Davis <ryand@zenspider.com>
2011-05-31 16:31:00 -0800

    prepping for release [git-p4: depot-paths = "//src/minitest/dev/": change = 6273]

--- lib/minitest/unit.rb
+++ lib/minitest/unit.rb
@@ -612,7 +612,7 @@ module MiniTest
   end
 
   class Unit
-    VERSION = "2.1.0" # :nodoc:
+    VERSION = "2.2.0" # :nodoc:
 
     attr_accessor :report, :failures, :errors, :skips # :nodoc:
     attr_accessor :test_count, :assertion_count       # :nodoc:
