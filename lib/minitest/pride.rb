924f66547a5d6dcacffee154937f2c9c19c14533
Ryan Davis <ryand@zenspider.com>
2011-07-01 14:12:02 -0800

    - Fix minitest/pride for MacRuby (ferrous26) [git-p4: depot-paths = "//src/minitest/dev/": change = 6431]

--- lib/minitest/pride.rb
+++ lib/minitest/pride.rb
@@ -6,20 +6,19 @@ require "minitest/unit"
 class PrideIO
   attr_reader :io
 
-  # stolen from /System/Library/Perl/5.10.0/Term/ANSIColor.pm
-  COLORS = (31..36).to_a
-  CHARS = ["*"]
-
   def initialize io
     @io = io
-    @colors = COLORS.cycle
-    @chars  = CHARS.cycle
+    # stolen from /System/Library/Perl/5.10.0/Term/ANSIColor.pm
+    @colors = (31..36).to_a
+    @size   = @colors.size
+    @index  = 0
   end
 
   def print o
     case o
     when "." then
-      io.print "\e[#{@colors.next}m#{@chars.next}\e[0m"
+      io.print "\e[#{@colors[@index % @size]}m*\e[0m"
+      @index += 1
     when "E", "F" then
       io.print "\e[41m\e[37m#{o}\e[0m"
     else
