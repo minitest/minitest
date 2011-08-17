3ac54f2e752ed47d63de801352cff6569fb53ca3
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-08-17 15:57:24 -0700

    Update implementation of .let, .described_class and .described_type.

--- lib/minitest/rspec.rb
+++ lib/minitest/rspec.rb
@@ -2,37 +2,39 @@ require 'minitest/spec'
 
 class MiniTest::Spec
 
+  def self.let(name, &block)
+    define_method(name) do
+      _let_memos.fetch(name) { |k| _let_memos[k] = instance_eval(&block) }
+    end
+  end
+
+  def self.subject(&block)
+    let(:subject, &block)
+  end
+
+  def _let_memos
+    @_let_memos ||= {}
+  end
+
+  def self.described_class
+    first_ancestor_desc_matching Class
+  end
+
+  def self.described_type
+    first_ancestor_desc_matching Module
+  end
+
+  def self.first_ancestor_desc_matching(type)
+    ancestors.each do |ancestor|
+      desc = ancestor.respond_to?(:desc) ? ancestor.desc : nil
+      return desc if desc && desc.kind_of?(type)
+    end
+
+    nil
+  end
+
   class << self
-    def let(name, &block)
-      ivar_name = "@__#{name}"
-
-      define_method(name) do
-        value = instance_variable_get(ivar_name) ||
-          instance_variable_set(ivar_name, [ instance_eval(&block) ])
-        value.first
-      end
-
-      public name
-    end
-
-    def subject(&block)
-      let(:subject, &block)
-    end
-
-    def described_class
-      ancestors.each do |ancestor|
-        return nil unless ancestor.respond_to?(:desc)
-        return ancestor.desc if Class === ancestor.desc
-      end
-    end
-
-    def described_type
-      ancestors.each do |ancestor|
-        return nil unless ancestor.respond_to?(:desc)
-        return ancestor.desc if Module === ancestor.desc
-      end
-    end
-
+    private :first_ancestor_desc_matching
   end
 
   def described_class
