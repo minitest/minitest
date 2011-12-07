fa13094efaa493a3efe0db76bb2d47c2cc35c583
Ryan Davis <ryand-ruby@zenspider.com>
2011-12-07 13:10:34 -0800

    + Added TestCase.exclude and load_excludes for programmatic filtering of tests. Refactored unit tests into metametameta to share assert_report, setup, and teardown [git-p4: depot-paths = "//src/minitest/dev/": change = 6785]

--- test/test_minitest_unit.rb
+++ test/test_minitest_unit.rb
@@ -1,12 +1,11 @@
-require 'stringio'
 require 'pathname'
-require 'minitest/autorun'
+require 'test/metametameta'
 
 module MyModule; end
 class AnError < StandardError; include MyModule; end
 class ImmutableString < String; def inspect; super.freeze; end; end
 
-class TestMiniTestUnit < MiniTest::Unit::TestCase
+class TestMiniTestUnit < MetaMetaMetaTestCase
   pwd = Pathname.new(File.expand_path(Dir.pwd))
   basedir = Pathname.new(File.expand_path("lib/minitest")) + 'mini'
   basedir = basedir.relative_path_from(pwd).to_s
@@ -15,38 +14,6 @@ class TestMiniTestUnit < MiniTest::Unit::TestCase
                "#{MINITEST_BASE_DIR}/test.rb:158:in `each'",
                "#{MINITEST_BASE_DIR}/test.rb:139:in `run'",
                "#{MINITEST_BASE_DIR}/test.rb:106:in `run'"]
-
-  def assert_report expected = nil
-    expected ||= "Run options: --seed 42
-
-# Running tests:
-
-.
-
-Finished tests in 0.00
-
-1 tests, 1 assertions, 0 failures, 0 errors, 0 skips
-"
-    output = @output.string.sub(/Finished tests in .*/, "Finished tests in 0.00")
-    output.sub!(/Loaded suite .*/, 'Loaded suite blah')
-    output.sub!(/^(\s+)(?:#{Regexp.union(__FILE__, File.expand_path(__FILE__))}):\d+:/o, '\1FILE:LINE:')
-    output.sub!(/\[(?:#{Regexp.union(__FILE__, File.expand_path(__FILE__))}):\d+\]/o, '[FILE:LINE]')
-    assert_equal(expected, output)
-  end
-
-  def setup
-    srand 42
-    MiniTest::Unit::TestCase.reset
-    @tu = MiniTest::Unit.new
-    @output = StringIO.new("")
-    MiniTest::Unit.runner = nil # protect the outer runner from the inner tests
-    MiniTest::Unit.output = @output
-  end
-
-  def teardown
-    MiniTest::Unit.output = $stdout
-    Object.send :remove_const, :ATestCase if defined? ATestCase
-  end
 
   def test_class_puke_with_assertion_failed
     exception = MiniTest::Assertion.new "Oh no!"
@@ -229,21 +196,23 @@ Finished tests in 0.00
 
     @tu.run %w[--seed 42]
 
-    expected = "Run options: --seed 42
+    expected = <<-EOM.gsub(/^ {6}/, '')
+      Run options: --seed 42
 
-# Running tests:
+      # Running tests:
 
-E.
+      E.
 
-Finished tests in 0.00
+      Finished tests in 0.00
 
-  1) Error:
-test_error(ATestCase):
-RuntimeError: unhandled exception
-    FILE:LINE:in `test_error'
+        1) Error:
+      test_error(ATestCase):
+      RuntimeError: unhandled exception
+          FILE:LINE:in `test_error'
 
-2 tests, 1 assertions, 0 failures, 1 errors, 0 skips
-"
+      2 tests, 1 assertions, 0 failures, 1 errors, 0 skips
+    EOM
+
     assert_report expected
   end
 
@@ -621,6 +590,8 @@ end
 
 class TestMiniTestUnitTestCase < MiniTest::Unit::TestCase
   def setup
+    super
+
     MiniTest::Unit::TestCase.reset
 
     @tc = MiniTest::Unit::TestCase.new 'fake tc'
