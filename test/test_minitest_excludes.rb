c135d948f18383b822453d4fac5d3664ced1b8ec
Charles Oliver Nutter <headius@headius.com>
2011-12-17 00:38:00 -0600

    Use a benign character to replace : in exclude filenames.

--- test/test_minitest_excludes.rb
+++ test/test_minitest_excludes.rb
@@ -51,4 +51,56 @@ class TestMiniTestExcludes < MetaMetaMetaTestCase
   ensure
     MiniTest::Unit::TestCase::EXCLUDE_DIR.replace(old_exclude_base)
   end
+  
+  def test_nested_cls_excludes
+    srand 42
+    old_exclude_base = MiniTest::Unit::TestCase::EXCLUDE_DIR
+
+    @assertion_count = 0
+
+    Dir.mktmpdir do |path|
+      MiniTest::Unit::TestCase::EXCLUDE_DIR.replace(path)
+      File.open File.join(path, "Nested@@ATestCase.rb"), "w" do |f|
+        f.puts <<-EOM
+          exclude :test_test2, "because it is borked"
+        EOM
+      end
+
+      tc = Class.new(MiniTest::Unit::TestCase) do
+        def test_test1; assert true  end
+        def test_test2; assert false end # oh noes!
+        def test_test3; assert true  end
+      end
+
+      nest = Module.new
+      Object.const_set(:Nested, nest)
+      nest.const_set(:ATestCase, tc)
+
+      assert_equal %w(test_test1 test_test2 test_test3), Nested::ATestCase.test_methods
+
+      @tu.run %w[--seed 42 --verbose]
+
+      expected = <<-EOM.gsub(/^ {8}/, '')
+        Run options: --seed 42 --verbose
+
+        # Running tests:
+
+        Nested::ATestCase#test_test2 = 0.00 s = S
+        Nested::ATestCase#test_test1 = 0.00 s = .
+        Nested::ATestCase#test_test3 = 0.00 s = .
+
+
+        Finished tests in 0.00
+
+          1) Skipped:
+        test_test2(Nested::ATestCase) [FILE:LINE]:
+        because it is borked
+
+        3 tests, 2 assertions, 0 failures, 0 errors, 1 skips
+      EOM
+      assert_report expected
+    end
+  ensure
+    MiniTest::Unit::TestCase::EXCLUDE_DIR.replace(old_exclude_base)
+  end
 end
