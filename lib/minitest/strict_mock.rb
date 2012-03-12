c3dc89f625638f202a189b6a5d3b74e5296c4eec
Cainã Costa <cainan.costa@gmail.com>
2012-03-11 23:56:58 -0300

    Adds RDoc documentation to StrictMock

--- lib/minitest/strict_mock.rb
+++ lib/minitest/strict_mock.rb
@@ -1,12 +1,42 @@
 require 'minitest/mock'
 
+##
+# A strict subset of MiniTest::Mock.
+#
+# In isolated tests, a StrictMock is no different than a common mock.
+# The only difference is when the test is called on a not-isolated
+# environment. It checks for the presence of the method on the mocked
+# class, and fails if it isn't there. This adds another layer of
+# security for full-suite tests, without compromising the isolation of unit
+# tests.
+
 class MiniTest::StrictMock < MiniTest::Mock
-  def initialize(constant)
+  def initialize(constant) # :nodoc:
     @constant_name = constant
     @constant = constantize(constant)
 
     super()
   end
+
+  ##
+  # Expect that method +name+ is called, optionally with +args+, and returns
+  # +retval+.
+  #
+  # +args+ is compared to the expected args using case equality (ie, the
+  # '===' operator), allowing for less specific expectations.
+  #
+  # If the mocked constant is defined, it algo checks for method presence
+  # and arity, and fails if the method don't exist or the arity is different.
+  #
+  #   class DefinedConstant
+  #     def defined_method(foo, bar)
+  #     end
+  #   end
+  #
+  #   @mock = MiniTest::StrictMock.new("StrictConstant")
+  #   @mock.expect(:undefined_method) # raises MockExpectationError
+  #   @mock.expect(:defined_method, [:foo]) # raises MockExpectationError
+  #
 
   def expect(name, retval, args = [])
     method = @constant.instance_method(name) rescue nil
@@ -23,7 +53,6 @@ class MiniTest::StrictMock < MiniTest::Mock
   end
 
   private
-  # Borrowed from ActiveSupport.
   def constantize(camel_cased_word)
     names = camel_cased_word.split('::')
     names.shift if names.empty? || names.first.empty?
