176f917ab813ce6dc31f93ba1daf082d7c02f821
Ryan Davis <ryand-ruby@zenspider.com>
2012-06-13 13:23:52 -0800

    + Removed generated method name munging from minitest/spec. (ordinaryzelig/tenderlove) [git-p4: depot-paths = "//src/minitest/dev/": change = 7541]

--- lib/minitest/spec.rb
+++ lib/minitest/spec.rb
@@ -174,12 +174,6 @@ class MiniTest::Spec < MiniTest::Unit::TestCase
     add_teardown_hook {|tc| tc.instance_eval(&block) }
   end
 
-  NAME_RE = if RUBY_VERSION >= "1.9"
-              Regexp.new("[^[[:word:]]]+")
-            else
-              /\W+/u
-            end
-
   ##
   # Define an expectation with name +desc+. Name gets morphed to a
   # proper test method name. For some freakish reason, people who
@@ -197,7 +191,7 @@ class MiniTest::Spec < MiniTest::Unit::TestCase
     @specs ||= 0
     @specs += 1
 
-    name = "test_%04d_%s" % [ @specs, desc.gsub(NAME_RE, '_').downcase ]
+    name = "test_%04d_%s" % [ @specs, desc ]
 
     define_method name, &block
 
