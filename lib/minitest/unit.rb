minitest/minitest

commit ef5507bbc35de4ade48ad1a0f350c21d9d1ecd6e

refactored to reduce flay score. marked some stuff as nodoc because rdoc kinda sucks (STILL) [git-p4: depot-paths = "//src/minitest/dev/": change = 7650]

lib/minitest/unit.rb
@@ -1239,7 +1239,11 @@ module MiniTest
       end
 
       def run_setup_hooks # :nodoc:
-        self.class.setup_hooks.each do |hook|
+        _run_hooks self.class.setup_hooks
+      end
+
+      def _run_hooks hooks # :nodoc:
+        hooks.each do |hook|
           if hook.respond_to?(:arity) && hook.arity == 1
             hook.call(self)
           else
@@ -1288,13 +1292,7 @@ module MiniTest
       end
 
       def run_teardown_hooks # :nodoc:
-        self.class.teardown_hooks.reverse.each do |hook|
-          if hook.respond_to?(:arity) && hook.arity == 1
-            hook.call(self)
-          else
-            hook.call
-          end
-        end
+        _run_hooks self.class.teardown_hooks
       end
 
       include MiniTest::Assertions
@@ -1302,7 +1300,7 @@ module MiniTest
   end # class Unit
 end # module MiniTest
 
-Minitest = MiniTest # because ugh... I typo this all the time
+Minitest = MiniTest # :nodoc: because ugh... I typo this all the time
 
 if $DEBUG then
   module Test                # :nodoc:

lib/minitest/mock.rb
@@ -50,7 +50,7 @@ module MiniTest
       self
     end
 
-    def __call name, data
+    def __call name, data # :nodoc:
       case data
       when Hash then
         "#{name}(#{data[:args].inspect[1..-2]}) => #{data[:retval].inspect}"

commit 2f52dd6454fa0b55551dcaaaade052bc4dccca6b

Teardown hooks should be called in reverse order (regression).
This fixes a regression introduced in
ef5507bbc35de4ade48ad1a0f350c21d9d1ecd6e - I was seeing a number of test
failures without this fix.

lib/minitest/unit.rb
@@ -1292,7 +1292,7 @@ module MiniTest
       end
 
       def run_teardown_hooks # :nodoc:
-        _run_hooks self.class.teardown_hooks
+        _run_hooks self.class.teardown_hooks.reverse
       end
 
       include MiniTest::Assertions