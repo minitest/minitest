0e3323fc0ade603ac9eaf06e24a2d9ebc820c399
Ryan Davis <ryand@zenspider.com>
2012-08-30 14:27:53 -0800

    + assert_output can now take regexps for expected values. (suggested by stomar) [git-p4: depot-paths = "//src/minitest/dev/": change = 7752]

--- lib/minitest/unit.rb
+++ lib/minitest/unit.rb
@@ -313,8 +313,11 @@ module MiniTest
         yield
       end
 
-      y = assert_equal stderr, err, "In stderr" if stderr
-      x = assert_equal stdout, out, "In stdout" if stdout
+      err_msg = Regexp === stderr ? :assert_match : :assert_equal if stderr
+      out_msg = Regexp === stdout ? :assert_match : :assert_equal if stdout
+
+      y = send err_msg, stderr, err, "In stderr" if err_msg
+      x = send out_msg, stdout, out, "In stdout" if out_msg
 
       (!stdout || x) && (!stderr || y)
     end
