4b73549b5d173a506588090fd5c686247dbbd2b1
Ryan Davis <ryand-ruby@zenspider.com>
2012-12-17 14:49:13 -0800

    Doco coverage [git-p4: depot-paths = "//src/minitest/dev/": change = 8063]

--- lib/minitest/parallel_each.rb
+++ lib/minitest/parallel_each.rb
@@ -1,8 +1,20 @@
+##
+# Provides a parallel #each that lets you enumerate using N threads.
+# Use environment variable N to customize. Defaults to 2. Enumerable,
+# so all the goodies come along (tho not all are wrapped yet to
+# return another ParallelEach instance).
+
 class ParallelEach
   require 'thread'
   include Enumerable
 
+  ##
+  # How many Threads to use for this parallel #each.
+
   N = (ENV['N'] || 2).to_i
+
+  ##
+  # Create a new ParallelEach instance over +list+.
 
   def initialize list
     @queue = Queue.new # *sigh*... the Queue api sucks sooo much...
@@ -11,9 +23,13 @@ class ParallelEach
     N.times { @queue << nil }
   end
 
-  def grep pattern
+  def grep pattern # :nodoc:
     self.class.new super
   end
+
+  ##
+  # Starts N threads that yield each element to your block. Joins the
+  # threads at the end.
 
   def each
     threads = N.times.map {
