fca6fff7a3cc7b144da2370c5ed882db873ca308
Ryan Davis <ryand@zenspider.com>
2013-05-14 16:12:55 -0800

    - Only load one plugin version per name. Tries for latest. [git-p4: depot-paths = "//src/minitest/dev/": change = 8554]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -68,9 +68,15 @@ module Minitest
   def self.load_plugins # :nodoc:
     return unless self.extensions.empty?
 
-    Gem.find_files("minitest/*_plugin.rb").each do |plugin_path|
-      require plugin_path
+    seen = {}
+
+    Gem.find_files("minitest/*_plugin.rb").reverse.each do |plugin_path|
       name = File.basename plugin_path, "_plugin.rb"
+
+      next if seen[name]
+      seen[name] = true
+
+      require plugin_path
       self.extensions << name
     end
   end
