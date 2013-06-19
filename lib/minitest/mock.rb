4035b170386bc601bb4b0a68626835d19e6809d1
Ryan Davis <ryand@zenspider.com>
2013-06-18 18:33:08 -0800

    - Fixed typo on minitest/mock rdoc. (guiceolin) [git-p4: depot-paths = "//src/minitest/dev/": change = 8696]

--- lib/minitest/mock.rb
+++ lib/minitest/mock.rb
@@ -42,7 +42,7 @@ module Minitest # :nodoc:
     #   @mock.uses_any_string("foo") # => true
     #   @mock.verify  # => true
     #
-    #   @mock.expect(:uses_one_string, true, ["foo"]
+    #   @mock.expect(:uses_one_string, true, ["foo"])
     #   @mock.uses_one_string("bar") # => true
     #   @mock.verify  # => raises MockExpectationError
 
