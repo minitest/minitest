f77c37152f6ff0748d29449ed17bb121894b0d94
Ryan Davis <ryand@zenspider.com>
2013-08-21 13:14:53 -0800

    - Require rubygems in load_plugins in case you're not using minitest/autorun. [git-p4: depot-paths = "//src/minitest/dev/": change = 8927]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -70,6 +70,8 @@ module Minitest
 
     seen = {}
 
+    require "rubygems" unless defined? Gem
+
     Gem.find_files("minitest/*_plugin.rb").each do |plugin_path|
       name = File.basename plugin_path, "_plugin.rb"
 
