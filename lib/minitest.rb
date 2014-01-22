2f324061705da788b7fb365f552f0ab671fc0be3
Ryan Davis <ryand@zenspider.com>
2014-01-22 14:56:47 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest/dev/": change = 9207]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -7,7 +7,7 @@ require "minitest/parallel"
 # :include: README.txt
 
 module Minitest
-  VERSION = "5.2.1" # :nodoc:
+  VERSION = "5.2.2" # :nodoc:
 
   @@installed_at_exit ||= false
   @@after_run = []
