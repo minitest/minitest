f08249ca6fcee2e20139c796027c4df9e538201b
Ryan Davis <ryand@zenspider.com>
2014-12-01 16:58:10 -0800

    + Allow seed to be given via ENV for rake test loader sadness: eg rake SEED=42. [git-p4: depot-paths = "//src/minitest/dev/": change = 9848]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -203,7 +203,7 @@ module Minitest
 
     unless options[:seed] then
       srand
-      options[:seed] = srand % 0xFFFF
+      options[:seed] = (ENV["SEED"] || srand).to_i % 0xFFFF
       orig_args << "--seed" << options[:seed].to_s
     end
 
