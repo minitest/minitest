3500da3d5eb78adea55e5fc0663300b050e40239
Ryan Davis <ryand@zenspider.com>
2014-12-19 16:32:12 -0800

    Fixed wonky indentation... enh-ruby-mode? [git-p4: depot-paths = "//src/minitest/dev/": change = 9888]

--- lib/minitest/assertions.rb
+++ lib/minitest/assertions.rb
@@ -402,17 +402,17 @@ module Minitest
     def capture_io
       _synchronize do
         begin
-      captured_stdout, captured_stderr = StringIO.new, StringIO.new
+          captured_stdout, captured_stderr = StringIO.new, StringIO.new
 
-      orig_stdout, orig_stderr = $stdout, $stderr
-      $stdout, $stderr         = captured_stdout, captured_stderr
+          orig_stdout, orig_stderr = $stdout, $stderr
+          $stdout, $stderr         = captured_stdout, captured_stderr
 
-      yield
+          yield
 
-      return captured_stdout.string, captured_stderr.string
-    ensure
-      $stdout = orig_stdout
-      $stderr = orig_stderr
+          return captured_stdout.string, captured_stderr.string
+        ensure
+          $stdout = orig_stdout
+          $stderr = orig_stderr
         end
       end
     end
