61b74093b8194c857e1e9789672cce29dc1179c3
Ryan Davis <ryand@zenspider.com>
2016-05-16 13:58:30 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest/dev/": change = 10643]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -7,7 +7,7 @@ require "minitest/parallel"
 # :include: README.rdoc
 
 module Minitest
-  VERSION = "5.8.4" # :nodoc:
+  VERSION = "5.9.0" # :nodoc:
   ENCS = "".respond_to? :encoding # :nodoc:
 
   @@installed_at_exit ||= false
