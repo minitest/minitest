8b2ee271d8b9ae292be662c72aa78966f7f59d90
Ryan Davis <ryand@zenspider.com>
2016-12-15 14:55:57 -0800

    - Fixed minitest/hell to use parallelize_me! (azul) [git-p4: depot-paths = "//src/minitest/dev/": change = 11154]

--- lib/minitest/hell.rb
+++ lib/minitest/hell.rb
@@ -1,13 +1,7 @@
 require "minitest/parallel"
 
 class Minitest::Test
-  class << self
-    alias :old_test_order :test_order # :nodoc:
-
-    def test_order # :nodoc:
-      :parallel
-    end
-  end
+  parallelize_me!
 end
 
 begin
