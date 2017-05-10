86481ca12116dbb6eca166b2f5f6cb316080fe6a
Ryan Davis <ryand@zenspider.com>
2017-05-10 13:20:54 -0800

    - Fixed verbose output on parallelize_me! classes. (chanks) [git-p4: depot-paths = "//src/minitest/dev/": change = 11319]

--- lib/minitest/parallel.rb
+++ lib/minitest/parallel.rb
@@ -29,6 +29,7 @@ module Minitest
             Thread.current.abort_on_exception = true
             while (job = queue.pop)
               klass, method, reporter = job
+              reporter.synchronize { reporter.prerecord klass, method }
               result = Minitest.run_one_method klass, method
               reporter.synchronize { reporter.record result }
             end
