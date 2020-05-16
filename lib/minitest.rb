32d49db55d80b8479237898f07d161bb52ef905c
Ryan Davis <ryand-ruby@zenspider.com>
2020-05-15 20:15:42 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest/dev/": change = 12654]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -8,7 +8,7 @@ require "stringio"
 # :include: README.rdoc
 
 module Minitest
-  VERSION = "5.14.0" # :nodoc:
+  VERSION = "5.14.1" # :nodoc:
   ENCS = "".respond_to? :encoding # :nodoc:
 
   @@installed_at_exit ||= false
