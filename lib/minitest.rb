a320274355de57cd70c4cab70cae5bc09c7e9708
Ryan Davis <ryand-ruby@zenspider.com>
2021-12-04 18:14:42 -0800

    + Use Etc.nprocessors by default in order to maximize cpu usage. (tonytonyjan) [git-p4: depot-paths = "//src/minitest/dev/": change = 13277]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -3,6 +3,7 @@ require "thread"
 require "mutex_m"
 require "minitest/parallel"
 require "stringio"
+require "etc"
 
 ##
 # :include: README.rdoc
@@ -23,7 +24,7 @@ module Minitest
   mc.send :attr_accessor, :parallel_executor
 
   warn "DEPRECATED: use MT_CPU instead of N for parallel test runs" if ENV["N"]
-  n_threads = (ENV["MT_CPU"] || ENV["N"] || 2).to_i
+  n_threads = (ENV["MT_CPU"] || ENV["N"] || Etc.nprocessors).to_i
   self.parallel_executor = Parallel::Executor.new n_threads
 
   ##
