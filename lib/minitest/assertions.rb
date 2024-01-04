4545d5c152b0bc994ec33e6ccd8e3771af97ac36
Ryan Davis <ryand-ruby@zenspider.com>
2024-01-04 12:43:29 -0800

    + Refactored location information in assertions, now using locations. [git-p4: depot-paths = "//src/minitest/dev/": change = 13993]

--- lib/minitest/assertions.rb
+++ lib/minitest/assertions.rb
@@ -198,6 +198,11 @@ module Minitest
       assert obj.empty?, msg
     end
 
+    def _where # :nodoc:
+      where = Minitest.filter_backtrace(caller_locations).first
+      [where.path, where.lineno].join ":"
+    end
+
     E = "" # :nodoc:
 
     ##
@@ -221,10 +226,7 @@ module Minitest
         if Minitest::VERSION =~ /^6/ then
           refute_nil exp, "Use assert_nil if expecting nil."
         else
-          where = Minitest.filter_backtrace(caller).first
-          where = where.split(/:in /, 2).first # clean up noise
-
-          warn "DEPRECATED: Use assert_nil if expecting nil from #{where}. This will fail in Minitest 6."
+          warn "DEPRECATED: Use assert_nil if expecting nil from #{_where}. This will fail in Minitest 6."
         end
       end
 
@@ -475,9 +477,7 @@ module Minitest
     # Fails unless the call returns a true value
 
     def assert_send send_ary, m = nil
-      where = Minitest.filter_backtrace(caller).first
-      where = where.split(/:in /, 2).first # clean up noise
-      warn "DEPRECATED: assert_send. From #{where}"
+      warn "DEPRECATED: assert_send. From #{_where}"
 
       recv, msg, *args = send_ary
       m = message(m) {
--- lib/minitest/spec.rb
+++ lib/minitest/spec.rb
@@ -13,8 +13,8 @@ class Module # :nodoc:
     # warn "%-22p -> %p %p" % [meth, new_name, dont_flip]
     self.class_eval <<-EOM, __FILE__, __LINE__ + 1
       def #{new_name} *args
-        where = Minitest.filter_backtrace(caller).first
-        where = where.split(/:in /, 2).first # clean up noise
+        where = Minitest.filter_backtrace(caller_locations).first
+        where = [where.path, where.lineno].join ":"
         Kernel.warn "DEPRECATED: global use of #{new_name} from #\{where}. Use #{target_obj}.#{new_name} instead. This will fail in Minitest 6."
         Minitest::Expectation.new(self, Minitest::Spec.current).#{new_name}(*args)
       end
