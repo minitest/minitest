15265da64bd93cbed6c1eecb4a85c88329297e37
Ryan Davis <ryand-ruby@zenspider.com>
2025-11-08 13:56:38 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest/dev/": change = 14597]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -10,7 +10,7 @@ require_relative "minitest/compress"
 # runtime. See +Minitest.run+ for more information.
 
 module Minitest
-  VERSION = "5.26.0" # :nodoc:
+  VERSION = "5.26.1" # :nodoc:
 
   @@installed_at_exit ||= false
   @@after_run = []
