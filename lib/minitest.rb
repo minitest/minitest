a52e7273a749e44b2d5a3398b56b6ed6e798a248
Ryan Davis <ryand-ruby@zenspider.com>
2025-11-17 13:49:34 -0800

    Fuuuuck I am SO tired of ruby 2.7! [git-p4: depot-paths = "//src/minitest/dev/": change = 14609]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -189,8 +189,12 @@ module Minitest
       end
 
       # omg wtf
-      def opts.short_alias(from, to) = top.short[to] = top.short[from]
-      def opts.long_alias(from, to)  = top.long[to]  = top.long[from]
+      def opts.short_alias(from, to)
+        top.short[to] = top.short[from]
+      end
+      def opts.long_alias(from, to)
+        top.long[to]  = top.long[from]
+      end
 
       # these will work but won't show up in --help output:
       opts.long_alias  "name", "include"
