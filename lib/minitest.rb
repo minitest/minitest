a8143f2297e4af06c88ceea4c0090d6c76309f37
Ryan Davis <ryand-ruby@zenspider.com>
2025-12-09 20:30:45 -0800

    - Removed obsolete conditional for prerecord [git-p4: depot-paths = "//src/minitest/dev/": change = 14668]

--- lib/minitest.rb
+++ lib/minitest.rb
@@ -1014,8 +1014,7 @@ module Minitest
 
     def prerecord klass, name # :nodoc:
       self.reporters.each do |reporter|
-        # TODO: remove conditional for minitest 6
-        reporter.prerecord klass, name if reporter.respond_to? :prerecord
+        reporter.prerecord klass, name
       end
     end
 
